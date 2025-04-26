efc663a6f8ac331c55aea7aac1981d446569e426
Никита Жарков <PORSCHIVEZ@MacBook-Pro-Nikita.local>
2025-04-26 20:37:12 +0300

    Correct main.tf: fix user-data indentation and SSH setup

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "yandex_container_repository" "bot" {
 }
 
 data "yandex_compute_image" "ubuntu" {
-  family      = "ubuntu-2204-lts"
+  family = "ubuntu-2204-lts"
 }
 
 resource "yandex_compute_instance" "bot_vm" {
@@ -43,18 +43,26 @@ resource "yandex_compute_instance" "bot_vm" {
   }
 
   metadata = {
+    enable-oslogin = "false"
     ssh-keys = "ubuntu:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCqjjCiO6Moj+OTV51QrGST1gjUyRDb7xModv8Lvog/9V5KI1hWjPdHoZn2ZeeNt17PbCPDOBO9TSCr44KjQ/+EhkzjYQGutgIejPIO7Yg5j5yQvkl/vtJ5QtLLfCZyCy6GjhOnp5k56G+/VLpOlPYZeizsBIBvf72XaUnD/WBYyxO2rxpgpMtcP51Vz2XgsxYo1hjJNjL6pUAo6s2swwRAOo5xSoRXtzjeZJzXJmZ0pzGbP0MThE+QLdaN7GA7u7OZfnHArOaikF6eJTnqHWjJ0D23RSCHowpmFTHCYrHkXu234p4y1DSFSHEzruJ+o9PkFSGA5jRdlQvZ+wP+qFYR n.zharkov92@yandex.ru"
+
     user-data = <<-EOC
-      #cloud-config
-      packages:
-        - docker.io
-      runcmd:
-        - docker login cr.yandex/${var.registry_id} -u oauth -p "${var.yc_token}"
-        - docker pull cr.yandex/${var.registry_id}/telegram-bot:latest
-        - docker run -d --restart unless-stopped \
-            -e TELEGRAM_TOKEN="${var.telegram_token}" \
-            --name telegram-bot \
-            cr.yandex/${var.registry_id}/telegram-bot:latest
-    EOC
+#cloud-config
+chpasswd:
+  list: |
+    ubuntu:SecureP@ssw0rd123
+  expire: false
+
+packages:
+  - docker.io
+
+runcmd:
+  - docker login cr.yandex/${var.registry_id} -u oauth -p "${var.yc_token}"
+  - docker pull cr.yandex/${var.registry_id}/telegram-bot:latest
+  - docker run -d --restart unless-stopped \
+      -e TELEGRAM_TOKEN="${var.telegram_token}" \
+      --name telegram-bot \
+      cr.yandex/${var.registry_id}/telegram-bot:latest
+EOC
   }
 }
